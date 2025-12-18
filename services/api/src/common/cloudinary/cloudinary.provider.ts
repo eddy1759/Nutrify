9ee8bd77b13e1c5774445d3fd39abfd18ba0cc3a
Edet Asuquo <eddy1759@gmail.com>
2025-12-18 02:38:18 +0100

    feat: image upload using cloudinary added

---
+++ services/api/src/common/cloudinary/cloudinary.provider.ts
@@ -0,0 +1,14 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { ConfigService } from '@nestjs/config';
+
+export const CloudinaryProvider = {
+  provide: 'CLOUDINARY',
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => {
+    return cloudinary.config({
+      cloud_name: config.getOrThrow<string>('CLOUDINARY_CLOUD_NAME'),
+      api_key: config.getOrThrow<string>('CLOUDINARY_API_KEY'),
+      api_secret: config.getOrThrow('CLOUDINARY_API_SECRET'),
+    });
+  },
+};
