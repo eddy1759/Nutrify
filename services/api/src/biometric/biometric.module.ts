93332784ffb27b6bdc602b2747706272ce632426
Edet Asuquo <eddy1759@gmail.com>
2025-12-31 18:14:33 +0100

    feat: added the biometric listener to the module

--- services/api/src/biometric/biometric.module.ts
+++ services/api/src/biometric/biometric.module.ts
@@ -3,10 +3,12 @@ import { BiometricService } from './biometric.service';
 import { BiometricsController } from './biometric.controller';
 import { EncryptionModule } from 'src/encryption/encryption.module';
 import { GamificationModule } from 'src/gamification/gamification.module';
+import { BiometricEventsListener } from './biometric.listener';
 
 @Module({
   imports: [EncryptionModule, GamificationModule],
-  providers: [BiometricService],
+  providers: [BiometricService, BiometricEventsListener],
   controllers: [BiometricsController],
+  exports: [BiometricService],
 })
 export class BiometricModule {}
